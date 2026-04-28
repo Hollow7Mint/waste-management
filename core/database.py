cacbd7a2d11ed6753325335e37d1aff7f84ef155
James Holloway <james.holloway94@gmail.com>
2026-04-28 07:40:34 +0000

    carry waf ip set sync stamp to config

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-04-28 07:04:56 UTC
+# Last sync: 2026-04-28 07:40:34 UTC
