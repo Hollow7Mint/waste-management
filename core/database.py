ecc1cf0b33d932b5ad5c7578c9bb2d0ed578d8c5
James Holloway <james.holloway94@gmail.com>
2026-04-28 11:59:06 +0000

    index s3 cors policy sync during audit pass

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-04-28 10:27:03 UTC
+# Last sync: 2026-04-28 11:59:06 UTC
