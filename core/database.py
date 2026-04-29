0706080ffda88f2692adcf3e4b2ce7a609c6dc50
James Holloway <james.holloway94@gmail.com>
2026-04-29 12:59:21 +0000

    capture rds snapshot completion time in record

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-04-29 11:35:56 UTC
+# Last sync: 2026-04-29 12:59:21 UTC
