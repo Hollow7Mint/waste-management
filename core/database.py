5340c044809c91bb758ae8a84544a1ebfccb50e3
James Holloway <james.holloway94@gmail.com>
2026-04-30 13:25:43 +0000

    annotate ec2 ami bake time for deploy record

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-04-30 12:11:04 UTC
+# Last sync: 2026-04-30 13:25:43 UTC
