1064d0235379b955aef739748a433ab4f7c64917
James Holloway <james.holloway94@gmail.com>
2026-04-30 15:18:30 +0000

    write mongodb index rebuild time into metadata

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-04-30 14:23:14 UTC
+# Last sync: 2026-04-30 15:18:30 UTC
