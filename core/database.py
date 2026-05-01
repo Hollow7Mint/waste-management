2dc1a9205f4ee6af3ff3831edf1d9ac5e38629c6
James Holloway <james.holloway94@gmail.com>
2026-05-01 11:41:12 +0000

    ship parameter store refresh record after nightly run

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-01 10:51:41 UTC
+# Last sync: 2026-05-01 11:41:12 UTC
