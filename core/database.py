5a1f13464da02033d340334966863916d8cd1594
James Holloway <james.holloway94@gmail.com>
2026-05-06 15:42:09 +0000

    record kms key rotation time in config banner

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-06 14:59:12 UTC
+# Last sync: 2026-05-06 15:42:08 UTC
