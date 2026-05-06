ca6809a89a3a641fd4d60642b18f305a24428da5
James Holloway <james.holloway94@gmail.com>
2026-05-06 12:53:03 +0000

    load alb routing rule timestamp from pipeline

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-06 11:20:09 UTC
+# Last sync: 2026-05-06 12:53:03 UTC
