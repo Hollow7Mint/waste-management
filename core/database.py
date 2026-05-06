e6dffe81bd37058bab30e0f8627637bd335f6968
James Holloway <james.holloway94@gmail.com>
2026-05-06 02:57:46 +0000

    frame mysql connection limit sync in config

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-06 01:18:16 UTC
+# Last sync: 2026-05-06 02:57:46 UTC
