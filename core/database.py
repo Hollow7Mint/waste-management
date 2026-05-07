f4f0a4f3cf06ae110fc7bfe41a3b679be2b77f68
James Holloway <james.holloway94@gmail.com>
2026-05-07 07:21:52 +0000

    validate mysql replication time in cluster config

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-07 06:14:12 UTC
+# Last sync: 2026-05-07 07:21:52 UTC
