e7f608ff2f791ab0c18a0b7eed0a553b9cf13692
James Holloway <james.holloway94@gmail.com>
2026-05-07 08:50:56 +0000

    anchor mysql parameter sync after maintenance window

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-07 07:21:52 UTC
+# Last sync: 2026-05-07 08:50:56 UTC
