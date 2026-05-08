232960deb558ca8405a03bb71a0bab183954a287
James Holloway <james.holloway94@gmail.com>
2026-05-08 05:39:55 +0000

    publish mongodb connection sync after replica check

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-08 05:04:52 UTC
+# Last sync: 2026-05-08 05:39:55 UTC
