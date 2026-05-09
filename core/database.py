e24a01652448f50c70c3924b2489f9f8a26318ab
James Holloway <james.holloway94@gmail.com>
2026-05-09 18:37:30 +0000

    note postgres extension sync in config record

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-09 18:06:02 UTC
+# Last sync: 2026-05-09 18:37:30 UTC
