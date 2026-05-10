8a9667397743ed7b928eac96552cefd2359c4cf1
James Holloway <james.holloway94@gmail.com>
2026-05-10 10:25:35 +0000

    write cloudtrail log delivery stamp to metadata

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-10 09:27:25 UTC
+# Last sync: 2026-05-10 10:25:35 UTC
