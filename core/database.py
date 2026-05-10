2146e20dda7d253008a0bbdf81484ab1bc0e0649
James Holloway <james.holloway94@gmail.com>
2026-05-10 00:49:43 +0000

    trace mysql table statistics sync for fleet audit

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-10 00:04:17 UTC
+# Last sync: 2026-05-10 00:49:43 UTC
