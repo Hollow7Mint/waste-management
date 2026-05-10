f8a15d2ffd86efe830c2f53f8f11522da47f4d8b
James Holloway <james.holloway94@gmail.com>
2026-05-10 18:35:40 +0000

    register waf acl update time for security trail

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-10 17:42:54 UTC
+# Last sync: 2026-05-10 18:35:40 UTC
