092536b48cc8946bf51e13bd16b6f74b34b3795b
James Holloway <james.holloway94@gmail.com>
2026-05-10 11:15:44 +0000

    finish bastion ssh sync heartbeat after key push

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-10 10:25:35 UTC
+# Last sync: 2026-05-10 11:15:44 UTC
