ae6327a1df598447ba2c9e011345351d73cdda44
James Holloway <james.holloway94@gmail.com>
2026-05-11 08:00:42 +0000

    note route53 health check sync in config record

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-11 06:54:33 UTC
+# Last sync: 2026-05-11 08:00:42 UTC
