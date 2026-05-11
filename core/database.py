48042ff9b7bc95dbd3e15acbee8e4138cb445332
James Holloway <james.holloway94@gmail.com>
2026-05-11 02:39:16 +0000

    post postgres replication heartbeat to config

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-11 01:59:19 UTC
+# Last sync: 2026-05-11 02:39:16 UTC
