d99b1b4ab33c0e00a33e3b2e8e236724a4786909
James Holloway <james.holloway94@gmail.com>
2026-05-11 08:15:59 +0000

    advance mysql gtid sync into config record

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-11 08:00:42 UTC
+# Last sync: 2026-05-11 08:15:59 UTC
