58e96af3eab1f1ec79645ba4dfc3b282a9ad10ef
James Holloway <james.holloway94@gmail.com>
2026-05-12 17:48:04 +0000

    restore mysql binlog time for replication audit

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-12 16:43:26 UTC
+# Last sync: 2026-05-12 17:48:04 UTC
