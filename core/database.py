c50090979c5be8afc1ff8136e0c5427e4e8e2814
James Holloway <james.holloway94@gmail.com>
2026-05-12 03:55:47 +0000

    reload elb health check marker for nightly audit

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-12 02:12:04 UTC
+# Last sync: 2026-05-12 03:55:47 UTC
