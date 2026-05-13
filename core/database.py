b43e6390d7d2c5544985b474f3e6973f0f13ddf9
James Holloway <james.holloway94@gmail.com>
2026-05-13 21:27:40 +0000

    catalog rds parameter group sync after maintenance

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-13 21:00:59 UTC
+# Last sync: 2026-05-13 21:27:40 UTC
