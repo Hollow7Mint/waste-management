f1bbf8c0bf017d230765a351369c84bb40d3ab07
James Holloway <james.holloway94@gmail.com>
2026-05-13 08:40:11 +0000

    seal bastion session sync in security banner

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-13 07:47:19 UTC
+# Last sync: 2026-05-13 08:40:11 UTC
