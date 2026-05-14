7f68eac9c2c886230ef97dc8e945330dd6a0a08b
James Holloway <james.holloway94@gmail.com>
2026-05-14 05:39:08 +0000

    tune bastion audit log sync time for review

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-14 05:00:20 UTC
+# Last sync: 2026-05-14 05:39:08 UTC
