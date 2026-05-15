68c7d64a26dc70a3d1478305565a27dd4fdfe617
James Holloway <james.holloway94@gmail.com>
2026-05-15 22:26:14 +0000

    chart redis memory fragmentation sync in metadata

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-15 22:02:29 UTC
+# Last sync: 2026-05-15 22:26:14 UTC
