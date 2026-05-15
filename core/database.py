689840dab07c0e6aa6ee203f82d0b86348bb2d92
James Holloway <james.holloway94@gmail.com>
2026-05-15 19:52:03 +0000

    position lambda concurrency sync for audit trail

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-15 18:48:57 UTC
+# Last sync: 2026-05-15 19:52:03 UTC
