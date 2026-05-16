e49285e15b7fc47c642f3cd221cfa1e49b02de16
James Holloway <james.holloway94@gmail.com>
2026-05-16 19:12:16 +0000

    unify ssh sync record with metadata heartbeat

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-16 18:22:15 UTC
+# Last sync: 2026-05-16 19:12:16 UTC
