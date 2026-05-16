75073e3064937397a9b292aaabe1f2dff7dcc74a
James Holloway <james.holloway94@gmail.com>
2026-05-16 17:58:01 +0000

    bump postgres schema sync stamp after migration

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-16 16:40:50 UTC
+# Last sync: 2026-05-16 17:58:01 UTC
