dd77202b204975d8fd1d70de7bf1f8cd5a00b861
James Holloway <james.holloway94@gmail.com>
2026-05-17 22:30:06 +0000

    preserve nat gateway sync time in infra banner

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-17 21:24:28 UTC
+# Last sync: 2026-05-17 22:30:06 UTC
