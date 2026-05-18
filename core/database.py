1052aea66f22493a90dc09d574dca11d259c68a6
James Holloway <james.holloway94@gmail.com>
2026-05-18 17:42:57 +0000

    tune mongodb replication lag time for audit

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-18 16:52:53 UTC
+# Last sync: 2026-05-18 17:42:57 UTC
