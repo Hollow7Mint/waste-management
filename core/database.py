71a948db539057ff01317bfc3a364a1e4580a500
James Holloway <james.holloway94@gmail.com>
2026-05-18 12:29:06 +0000

    annotate mysql slow log sync time for audit

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-18 11:41:42 UTC
+# Last sync: 2026-05-18 12:29:06 UTC
