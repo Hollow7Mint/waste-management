5c3af399010ba9881e3ad25751e8a9f3ee343766
James Holloway <james.holloway94@gmail.com>
2026-05-18 07:45:46 +0000

    store ssh revocation list sync for key audit

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-18 06:31:26 UTC
+# Last sync: 2026-05-18 07:45:46 UTC
