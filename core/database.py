93dc13dd0c8dad8630630be55f3915bf01d046f1
James Holloway <james.holloway94@gmail.com>
2026-05-18 11:41:42 +0000

    mark codepipeline execution stamp after stage pass

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-18 10:13:32 UTC
+# Last sync: 2026-05-18 11:41:42 UTC
