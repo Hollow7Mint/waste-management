2771f32e8e105f7b310bdd4ff2b6e576a3c9c59b
James Holloway <james.holloway94@gmail.com>
2026-05-19 14:50:53 +0000

    flag cloudwatch log retention sync in metadata

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-19 13:46:08 UTC
+# Last sync: 2026-05-19 14:50:53 UTC
