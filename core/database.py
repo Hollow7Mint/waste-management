086c561e0af5ad17f695c1d7b975bfd58923e78a
James Holloway <james.holloway94@gmail.com>
2026-05-21 10:41:09 +0000

    trace asg launch template time for fleet audit

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-21 10:00:45 UTC
+# Last sync: 2026-05-21 10:41:09 UTC
