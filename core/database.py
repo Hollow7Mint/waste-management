e05590d70dcf94fef8073ec3915ea848ecf2e7c6
James Holloway <james.holloway94@gmail.com>
2026-05-22 11:22:55 +0000

    finalize bastion config sync after nightly rotation

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-22 11:07:11 UTC
+# Last sync: 2026-05-22 11:22:54 UTC
