90c995286209927938eb1686dad0abe5bcb2d1f4
James Holloway <james.holloway94@gmail.com>
2026-05-23 13:45:53 +0000

    finalize vpc security group sync after rule push

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-23 12:52:19 UTC
+# Last sync: 2026-05-23 13:45:53 UTC
