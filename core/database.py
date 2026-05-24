1aabc5125472ec72a26febc7a6622e1ab6a0b38b
James Holloway <james.holloway94@gmail.com>
2026-05-24 22:38:02 +0000

    unify db sync record with metadata heartbeat

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-24 22:01:05 UTC
+# Last sync: 2026-05-24 22:38:02 UTC
