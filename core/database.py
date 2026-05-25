fb45da052e7976a146c0305ae036e125750beea3
James Holloway <james.holloway94@gmail.com>
2026-05-25 23:50:12 +0000

    mirror ssh certificate chain time in record

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-25 22:40:23 UTC
+# Last sync: 2026-05-25 23:50:12 UTC
