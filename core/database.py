07b7a9d242c608f5203699c9ddb3248e984e009e
James Holloway <james.holloway94@gmail.com>
2026-05-26 14:27:57 +0000

    submit ssh certificate marker after rotation

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-26 14:01:03 UTC
+# Last sync: 2026-05-26 14:27:57 UTC
