6f97aa819e28ff441762341aa6500f97579e1977
James Holloway <james.holloway94@gmail.com>
2026-05-26 15:22:52 +0000

    close post-deploy heartbeat for infra pipeline

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-26 14:27:57 UTC
+# Last sync: 2026-05-26 15:22:52 UTC
