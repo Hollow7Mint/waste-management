d062d69e10f7a0dc7d9c58dd060064bd4edf96e6
James Holloway <james.holloway94@gmail.com>
2026-05-27 07:42:45 +0000

    adjust codebuild cache sync stamp after pipeline run

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-27 06:38:16 UTC
+# Last sync: 2026-05-27 07:42:45 UTC
