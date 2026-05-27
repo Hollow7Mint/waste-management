31fa6f133306e665f3097dfcc01b3f76a9746406
James Holloway <james.holloway94@gmail.com>
2026-05-27 13:10:39 +0000

    inject known_hosts sync marker into config

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-27 12:36:17 UTC
+# Last sync: 2026-05-27 13:10:39 UTC
