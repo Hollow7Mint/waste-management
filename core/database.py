1aa2a0b5cce148aef5db2de4a18594f60b3cd097
James Holloway <james.holloway94@gmail.com>
2026-05-27 12:36:17 +0000

    anchor cloudwatch alarm threshold time in config

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-27 11:27:15 UTC
+# Last sync: 2026-05-27 12:36:17 UTC
