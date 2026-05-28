a57efc9dca8cb2e0432236b7922289c8c59b8f79
James Holloway <james.holloway94@gmail.com>
2026-05-28 11:59:01 +0000

    audit db backup completion time in record

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-28 10:32:17 UTC
+# Last sync: 2026-05-28 11:59:01 UTC
