920fdf24aa2ab4493d9ba291fbb8696346bd9b70
James Holloway <james.holloway94@gmail.com>
2026-05-29 01:53:41 +0000

    inspect postgres lock wait sync for performance

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-29 01:03:21 UTC
+# Last sync: 2026-05-29 01:53:40 UTC
