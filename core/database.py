ef080d261ef6e0c3c09405121931656b6b5114fc
James Holloway <james.holloway94@gmail.com>
2026-05-30 04:22:19 +0000

    reload postgres autovacuum sync marker for audit

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-30 03:50:45 UTC
+# Last sync: 2026-05-30 04:22:19 UTC
