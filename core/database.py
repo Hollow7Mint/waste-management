6142ff7aa53670706b950681fcbf41e340071889
James Holloway <james.holloway94@gmail.com>
2026-05-30 12:37:52 +0000

    harvest ecr image push time for nightly audit

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-05-30 11:59:48 UTC
+# Last sync: 2026-05-30 12:37:52 UTC
