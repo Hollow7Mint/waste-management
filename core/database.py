1ede0b7514c07dc1444138991e180b40b23ac627
James Holloway <james.holloway94@gmail.com>
2026-06-01 15:39:36 +0000

    recheck bastion connectivity sync after maintenance

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-01 14:28:56 UTC
+# Last sync: 2026-06-01 15:39:36 UTC
