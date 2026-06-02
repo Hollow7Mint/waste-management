5b32f273994779f8b233814671877b7061428a53
James Holloway <james.holloway94@gmail.com>
2026-06-02 06:29:36 +0000

    tag known_hosts sync checkpoint for key trail

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-02 05:55:07 UTC
+# Last sync: 2026-06-02 06:29:36 UTC
