7ca3c4cea37086ab8faaf968881274cbe9b408b1
James Holloway <james.holloway94@gmail.com>
2026-06-02 15:44:11 +0000

    inspect iam trust policy sync marker for review

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-02 14:16:01 UTC
+# Last sync: 2026-06-02 15:44:11 UTC
