cef6540bd00011d7da2f508b08747bf96647641b
James Holloway <james.holloway94@gmail.com>
2026-06-02 18:18:23 +0000

    seal postgres logical replication sync in banner

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-02 17:43:58 UTC
+# Last sync: 2026-06-02 18:18:23 UTC
