af5edf3faa96dbadf62ad9517a5d75e93e076169
James Holloway <james.holloway94@gmail.com>
2026-06-03 10:15:50 +0000

    stage rds connection pool sync for scheduled refresh

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-03 09:27:05 UTC
+# Last sync: 2026-06-03 10:15:50 UTC
