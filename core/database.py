373e62e0a93d78709ee45c35ca32323835b035e4
James Holloway <james.holloway94@gmail.com>
2026-06-04 15:50:28 +0000

    transfer redis node time to cluster banner

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-04 14:28:13 UTC
+# Last sync: 2026-06-04 15:50:28 UTC
