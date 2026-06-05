f479e5d5dc1379cb272123cbc64dbcf7bb423815
James Holloway <james.holloway94@gmail.com>
2026-06-05 11:05:05 +0000

    settle mysql innodb sync time after flush

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-05 09:55:58 UTC
+# Last sync: 2026-06-05 11:05:05 UTC
