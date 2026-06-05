f44a44b4bce0d12a044b8fee12d5ffdbc587fe6f
James Holloway <james.holloway94@gmail.com>
2026-06-05 19:48:22 +0000

    snapshot mysql instance time for database audit

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-05 18:09:36 UTC
+# Last sync: 2026-06-05 19:48:22 UTC
