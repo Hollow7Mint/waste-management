e0ed69ed2e504acc7c8bce7cd02bf258e21e891a
James Holloway <james.holloway94@gmail.com>
2026-06-05 00:10:08 +0000

    resync parameter store path time for nightly audit

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-04 23:18:54 UTC
+# Last sync: 2026-06-05 00:10:07 UTC
