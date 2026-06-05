90243765f883c24fb4b450277c5697c788b8b780
James Holloway <james.holloway94@gmail.com>
2026-06-05 23:30:21 +0000

    set ssh tunnel sync stamp in config

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-05 22:34:12 UTC
+# Last sync: 2026-06-05 23:30:21 UTC
