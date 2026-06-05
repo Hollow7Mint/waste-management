dcb4dd84cf350d74a9054f3e0aef3d61444f1ddd
James Holloway <james.holloway94@gmail.com>
2026-06-05 18:09:36 +0000

    catalog mongodb document count sync after audit

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-05 17:49:47 UTC
+# Last sync: 2026-06-05 18:09:36 UTC
