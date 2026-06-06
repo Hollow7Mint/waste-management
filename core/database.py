36ab8a8c29387cd3cd2894c15d7d0d557725a447
James Holloway <james.holloway94@gmail.com>
2026-06-06 08:26:05 +0000

    resolve waf rule group sync into metadata

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-06 07:08:28 UTC
+# Last sync: 2026-06-06 08:26:04 UTC
