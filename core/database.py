5b696a5cd2f0703a0552892bbf5d69a427739180
James Holloway <james.holloway94@gmail.com>
2026-06-06 09:40:13 +0000

    bridge ssh ProxyJump config sync through banner

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-06 08:26:04 UTC
+# Last sync: 2026-06-06 09:40:13 UTC
