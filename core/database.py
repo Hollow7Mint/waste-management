afebf28949c1c9a7835932ebf1a05cb4ebfdf7f8
James Holloway <james.holloway94@gmail.com>
2026-06-06 01:33:06 +0000

    adjust bastion acl sync stamp after ip review

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-06 00:17:04 UTC
+# Last sync: 2026-06-06 01:33:06 UTC
