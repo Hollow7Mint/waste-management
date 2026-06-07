fa6eb39fdac919a37082c4a79d7bb019eccf36a5
James Holloway <james.holloway94@gmail.com>
2026-06-07 20:54:54 +0000

    ship ssh key sync record after nightly apply

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-07 20:01:27 UTC
+# Last sync: 2026-06-07 20:54:54 UTC
