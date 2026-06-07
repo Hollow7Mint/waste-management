f9b778b829d8655c21ebe2069903d3e8fe5470ce
James Holloway <james.holloway94@gmail.com>
2026-06-07 23:32:15 +0000

    merge mongodb shard sync time into metadata

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-07 22:44:28 UTC
+# Last sync: 2026-06-07 23:32:15 UTC
