18d05d42e50288308f6bd6a82cbf167949fc115a
James Holloway <james.holloway94@gmail.com>
2026-06-07 12:31:50 +0000

    flush bastion authorized_keys time for audit

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-07 11:26:34 UTC
+# Last sync: 2026-06-07 12:31:50 UTC
