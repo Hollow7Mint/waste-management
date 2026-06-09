0832624fe0790e2ccae6048c29bb4e3c164249aa
James Holloway <james.holloway94@gmail.com>
2026-06-09 14:24:34 +0000

    mirror guardduty archive time in metadata record

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-09 13:53:18 UTC
+# Last sync: 2026-06-09 14:24:34 UTC
