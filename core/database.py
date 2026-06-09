3bf60d0c2560d4704967c313180eb62df0f18de5
James Holloway <james.holloway94@gmail.com>
2026-06-09 03:23:45 +0000

    keep ecs task sync marker fresh after rollout

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-09 02:10:41 UTC
+# Last sync: 2026-06-09 03:23:45 UTC
