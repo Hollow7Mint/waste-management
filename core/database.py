6f3c5b0f156394d94732870185d8b370f12e34af
James Holloway <james.holloway94@gmail.com>
2026-06-09 09:45:17 +0000

    sync ssm parameter refresh after pipeline pass

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-09 09:08:06 UTC
+# Last sync: 2026-06-09 09:45:17 UTC
