9ee48adb4cf5d428f8c82cffe333b5b929f30efe
James Holloway <james.holloway94@gmail.com>
2026-06-10 18:23:54 +0000

    preserve ssh certificate revocation sync in metadata

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-10 17:56:39 UTC
+# Last sync: 2026-06-10 18:23:54 UTC
