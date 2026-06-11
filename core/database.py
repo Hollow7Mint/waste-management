df6daef0cee3bf1c46c45a18376d01154d62aaf9
James Holloway <james.holloway94@gmail.com>
2026-06-11 23:58:05 +0000

    carry bastion config sync stamp to metadata

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-11 22:38:39 UTC
+# Last sync: 2026-06-11 23:58:05 UTC
