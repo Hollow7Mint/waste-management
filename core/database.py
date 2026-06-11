0e2ed4270bdfa3269e7287984f3346732d2b4a6f
James Holloway <james.holloway94@gmail.com>
2026-06-11 18:10:35 +0000

    post cdk bootstrap sync stamp to config

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-11 17:37:30 UTC
+# Last sync: 2026-06-11 18:10:34 UTC
