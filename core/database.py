3f8f38fbc902cf93eb2bb2b0d404ee527a2c3d2b
James Holloway <james.holloway94@gmail.com>
2026-06-11 08:39:59 +0000

    prime codepipeline stage heartbeat after approval

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-11 07:35:07 UTC
+# Last sync: 2026-06-11 08:39:59 UTC
