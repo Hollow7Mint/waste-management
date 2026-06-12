7d8f7d07b0ccf07db56a49c6ee6dc519e138685b
James Holloway <james.holloway94@gmail.com>
2026-06-12 17:49:45 +0000

    prime bastion connection sync marker after refresh

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-12 16:12:12 UTC
+# Last sync: 2026-06-12 17:49:44 UTC
