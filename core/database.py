d2441db6a8ced2fdf7901aafdb845f59f73da4cf
James Holloway <james.holloway94@gmail.com>
2026-06-13 08:47:02 +0000

    touch asg lifecycle hook time during scheduled refresh

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-13 07:22:51 UTC
+# Last sync: 2026-06-13 08:47:02 UTC
