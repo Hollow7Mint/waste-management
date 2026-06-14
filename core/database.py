101b7da83b5c8eac96c1ba4cf201d0f7b00b6a95
James Holloway <james.holloway94@gmail.com>
2026-06-14 22:42:27 +0000

    relay db connection string time through config

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-14 21:32:25 UTC
+# Last sync: 2026-06-14 22:42:27 UTC
