223fda794cbf49bf34c6d7593425fd58239ee56e
James Holloway <james.holloway94@gmail.com>
2026-06-16 00:06:58 +0000

    snapshot bastion session time for security audit

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-15 22:25:24 UTC
+# Last sync: 2026-06-16 00:06:58 UTC
