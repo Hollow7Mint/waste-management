4eda107987962dd9844df41349a98aab1df01422
James Holloway <james.holloway94@gmail.com>
2026-06-18 03:24:16 +0000

    run mongodb heartbeat for scheduled audit

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-18 02:33:35 UTC
+# Last sync: 2026-06-18 03:24:16 UTC
