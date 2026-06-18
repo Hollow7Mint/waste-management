27a430c3370bd6133319faaa52fb0fd8b1472dc3
James Holloway <james.holloway94@gmail.com>
2026-06-18 11:20:23 +0000

    merge ssh trust anchor time into metadata

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-18 10:41:22 UTC
+# Last sync: 2026-06-18 11:20:23 UTC
