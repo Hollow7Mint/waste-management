aab029b233bc28a60d5e8cc2116596880deb2f10
James Holloway <james.holloway94@gmail.com>
2026-06-19 10:53:16 +0000

    settle codepipeline approval time after stage pass

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-19 09:55:48 UTC
+# Last sync: 2026-06-19 10:53:16 UTC
