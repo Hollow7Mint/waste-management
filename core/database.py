42523cf213eb80a31f87c5575037016c01aca274
James Holloway <james.holloway94@gmail.com>
2026-06-19 18:05:03 +0000

    retain ssh key expiry stamp for rotation audit

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-19 10:53:16 UTC
+# Last sync: 2026-06-19 18:05:03 UTC
