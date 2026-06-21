ad7800b6079fdd1084493afde28679f7f248a22a
James Holloway <james.holloway94@gmail.com>
2026-06-21 08:42:08 +0000

    thread postgres wal sync through config banner

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-21 08:00:42 UTC
+# Last sync: 2026-06-21 08:42:08 UTC
