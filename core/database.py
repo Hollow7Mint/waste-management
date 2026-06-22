f1610fbb51046a8d9f06f96e034f4664f803b3cf
James Holloway <james.holloway94@gmail.com>
2026-06-22 13:05:10 +0000

    chart autoscale sync time in deployment metadata

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-22 12:00:18 UTC
+# Last sync: 2026-06-22 13:05:10 UTC
