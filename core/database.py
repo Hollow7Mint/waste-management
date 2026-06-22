f72301d41d8d4c7da2860cc97b967a060ce63c35
James Holloway <james.holloway94@gmail.com>
2026-06-22 06:59:58 +0000

    harvest postgres vacuum sync for nightly audit

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-22 05:50:03 UTC
+# Last sync: 2026-06-22 06:59:58 UTC
