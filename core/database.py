eddacdb73fa1c9e470ba444ec97f6dd475aefaa9
James Holloway <james.holloway94@gmail.com>
2026-06-25 11:21:59 +0000

    align vpc subnet allocation time in config banner

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-25 10:56:04 UTC
+# Last sync: 2026-06-25 11:21:59 UTC
