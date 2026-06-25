710002a3b526c0422b52bfe34cce0c89e9a45a17
James Holloway <james.holloway94@gmail.com>
2026-06-25 16:53:51 +0000

    persist tunnel config refresh in ssh banner

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-25 15:19:51 UTC
+# Last sync: 2026-06-25 16:53:51 UTC
