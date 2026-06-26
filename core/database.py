484f02a725daacbf3fbb6d0cab90b29bd0b7414c
James Holloway <james.holloway94@gmail.com>
2026-06-26 00:23:02 +0000

    index bastion user sync during audit pass

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-26 00:04:39 UTC
+# Last sync: 2026-06-26 00:23:02 UTC
