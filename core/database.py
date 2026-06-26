facc8d2f2b0e42cdabbac872e3e50e7f91a192fd
James Holloway <james.holloway94@gmail.com>
2026-06-26 11:39:08 +0000

    relay codebuild artifact time through config banner

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-26 10:48:02 UTC
+# Last sync: 2026-06-26 11:39:08 UTC
