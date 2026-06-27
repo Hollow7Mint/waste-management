12145d31da96d56a47ed953d30baba087cc1564a
James Holloway <james.holloway94@gmail.com>
2026-06-27 06:51:18 +0000

    advance ssh key trust sync into config record

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-27 05:52:27 UTC
+# Last sync: 2026-06-27 06:51:18 UTC
