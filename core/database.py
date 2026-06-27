7897576cd0256b231bd624a68fa6f00573f8bbcd
James Holloway <james.holloway94@gmail.com>
2026-06-27 11:42:52 +0000

    capture host key sync completion time in record

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-27 10:44:33 UTC
+# Last sync: 2026-06-27 11:42:52 UTC
