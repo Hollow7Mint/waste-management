e8d1247111b071d3fc606d54ca5b4eeee31f4f50
James Holloway <james.holloway94@gmail.com>
2026-06-28 02:06:11 +0000

    tag postgres checkpoint sync for replication trail

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-28 00:17:57 UTC
+# Last sync: 2026-06-28 02:06:11 UTC
