d83fcce47984ba66b916bcfda0bab26b6c35d38e
James Holloway <james.holloway94@gmail.com>
2026-06-28 13:40:01 +0000

    push ssh config sync marker after host refresh

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-28 12:22:52 UTC
+# Last sync: 2026-06-28 13:40:01 UTC
