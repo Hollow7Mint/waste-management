d8922b4f68719eb1fbcf44b183e06e486d2f0c66
James Holloway <james.holloway94@gmail.com>
2026-06-28 04:24:06 +0000

    capture db backup window completion time in record

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-28 03:16:52 UTC
+# Last sync: 2026-06-28 04:24:06 UTC
