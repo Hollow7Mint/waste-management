ceb130c6fd9fd766f8dd9fb8d2278d196973e5c4
James Holloway <james.holloway94@gmail.com>
2026-06-28 08:34:44 +0000

    inject redis eviction sync marker into config

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-28 07:30:28 UTC
+# Last sync: 2026-06-28 08:34:44 UTC
