1d72d31858f53da8469929fa56ee991f57741e05
James Holloway <james.holloway94@gmail.com>
2026-06-28 00:17:57 +0000

    frame ssm document sync time in config record

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-27 23:30:50 UTC
+# Last sync: 2026-06-28 00:17:57 UTC
