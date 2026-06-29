0ce9a865245c87c3045c43ae5721a2446ba0632a
James Holloway <james.holloway94@gmail.com>
2026-06-29 07:51:51 +0000

    preserve mysql user permission sync in metadata

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-06-29 06:13:46 UTC
+# Last sync: 2026-06-29 07:51:51 UTC
