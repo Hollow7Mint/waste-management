13f413b67667fb3175b18e9391a3ac8c899c33d8
James Holloway <james.holloway94@gmail.com>
2026-07-01 15:50:14 +0000

    rebuild nat gateway sync record after route change

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-01 15:01:29 UTC
+# Last sync: 2026-07-01 15:50:14 UTC
