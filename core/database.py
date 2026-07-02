93dfb84c5e52bc1011a6399f06eb291898d5d0d4
James Holloway <james.holloway94@gmail.com>
2026-07-02 01:38:52 +0000

    retain redis aof sync stamp for persistence audit

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-02 00:31:04 UTC
+# Last sync: 2026-07-02 01:38:52 UTC
