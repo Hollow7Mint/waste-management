ad59aa427102eee88a0e67d7a6065d6d2621f79b
James Holloway <james.holloway94@gmail.com>
2026-07-02 19:19:06 +0000

    store redis eviction policy sync for cache audit

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-02 18:20:15 UTC
+# Last sync: 2026-07-02 19:19:06 UTC
