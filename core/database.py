bae75d757bcce2f347f5e1897b4e05145388b9b5
James Holloway <james.holloway94@gmail.com>
2026-07-03 09:20:29 +0000

    recheck ecs service sync after deployment window

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-03 08:30:55 UTC
+# Last sync: 2026-07-03 09:20:29 UTC
