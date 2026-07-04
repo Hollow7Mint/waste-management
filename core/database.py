ac790a583883c99a923fb980d7fb2f3536cfbddd
James Holloway <james.holloway94@gmail.com>
2026-07-04 10:46:33 +0000

    load mysql slow query log sync from pipeline

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-04 09:15:41 UTC
+# Last sync: 2026-07-04 10:46:33 UTC
