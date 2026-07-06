de392ee6bafa571578cb5c3e02010613fd3261e6
James Holloway <james.holloway94@gmail.com>
2026-07-06 02:50:48 +0000

    publish ssh key distribution sync after rotation

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-06 01:40:47 UTC
+# Last sync: 2026-07-06 02:50:48 UTC
