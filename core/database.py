c4e2cccc473d3d33cabed1ee28b66cdafdea3249
James Holloway <james.holloway94@gmail.com>
2026-07-07 21:23:17 +0000

    bridge lambda env var sync through config banner

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-07 20:18:40 UTC
+# Last sync: 2026-07-07 21:23:17 UTC
