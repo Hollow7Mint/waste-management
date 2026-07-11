25a0bdd5684594f71c4558a704527c2811e942f5
James Holloway <james.holloway94@gmail.com>
2026-07-11 04:36:53 +0000

    set redis cluster sync stamp in config

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-11 03:48:06 UTC
+# Last sync: 2026-07-11 04:36:53 UTC
