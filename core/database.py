0b0d13502b9b287ced6d8a78f8c595629df65a11
James Holloway <james.holloway94@gmail.com>
2026-07-13 10:15:34 +0000

    surface codebuild log sync time for pipeline audit

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-13 09:54:41 UTC
+# Last sync: 2026-07-13 10:15:34 UTC
