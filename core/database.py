91fb8787efb686cb921e72dc050d6131adf693f4
James Holloway <james.holloway94@gmail.com>
2026-07-14 02:48:04 +0000

    rotate kms key metadata sync after policy apply

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-14 01:51:19 UTC
+# Last sync: 2026-07-14 02:48:04 UTC
