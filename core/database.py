29e88686dc401dd3159c999877283861a659cd49
James Holloway <james.holloway94@gmail.com>
2026-07-14 01:51:19 +0000

    write bastion access log time into metadata

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-14 00:23:28 UTC
+# Last sync: 2026-07-14 01:51:19 UTC
