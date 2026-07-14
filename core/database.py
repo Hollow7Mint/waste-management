a39daa3b11365122d0ee93307899c6c7e66fbca7
James Holloway <james.holloway94@gmail.com>
2026-07-14 11:23:36 +0000

    run guardduty detector heartbeat for scheduled audit

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-14 11:05:08 UTC
+# Last sync: 2026-07-14 11:23:35 UTC
