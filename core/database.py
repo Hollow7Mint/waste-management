088dd9d0e11fc831d60bf75b1d0df29515b26882
James Holloway <james.holloway94@gmail.com>
2026-07-15 21:14:48 +0000

    chart ssh connection attempt sync in metadata

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-15 20:39:06 UTC
+# Last sync: 2026-07-15 21:14:48 UTC
