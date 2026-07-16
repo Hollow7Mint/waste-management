cc4c78f695c5632730d93b73a2e1865baeda68f2
James Holloway <james.holloway94@gmail.com>
2026-07-16 22:41:15 +0000

    load bastion session log sync from pipeline

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-16 21:31:26 UTC
+# Last sync: 2026-07-16 22:41:14 UTC
