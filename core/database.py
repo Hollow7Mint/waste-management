9464ad96038e03b3c23f3df364692c02f75ba74e
James Holloway <james.holloway94@gmail.com>
2026-07-16 09:38:03 +0000

    stage postgres tablespace sync for scheduled refresh

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-16 09:03:30 UTC
+# Last sync: 2026-07-16 09:38:03 UTC
