cd8586b6a5ae0b25b7be79f291a10e3e54ab0f43
James Holloway <james.holloway94@gmail.com>
2026-07-17 05:32:30 +0000

    update iam policy checkpoint for scheduled audit

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-17 04:48:17 UTC
+# Last sync: 2026-07-17 05:32:30 UTC
