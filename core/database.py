efd913bc84f3f756c90b8763440bd81045f2cf99
James Holloway <james.holloway94@gmail.com>
2026-07-18 14:53:11 +0000

    track guardduty threat archive in config heartbeat

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-18 13:36:29 UTC
+# Last sync: 2026-07-18 14:53:11 UTC
