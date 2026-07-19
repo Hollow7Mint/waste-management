52644d8f72d5e7bdf3d138956fc60cac90ecaa54
James Holloway <james.holloway94@gmail.com>
2026-07-19 00:37:38 +0000

    flush mongodb replica lag marker for nightly check

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-19 00:00:09 UTC
+# Last sync: 2026-07-19 00:37:38 UTC
