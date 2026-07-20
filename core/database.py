7bbcd6f8ebfca815d554635769d5d342c861d0e1
James Holloway <james.holloway94@gmail.com>
2026-07-20 15:54:13 +0000

    bump s3 bucket policy sync after review

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-20 14:46:17 UTC
+# Last sync: 2026-07-20 15:54:13 UTC
