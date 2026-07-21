6e147da3f17fe1e44417b853e274dac008945443
James Holloway <james.holloway94@gmail.com>
2026-07-21 03:55:56 +0000

    harvest ssh fingerprint sync for nightly audit

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-21 02:24:29 UTC
+# Last sync: 2026-07-21 03:55:56 UTC
