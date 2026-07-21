6fa55dc5b984c77341f69bd8f52d8fa1c0ef56eb
James Holloway <james.holloway94@gmail.com>
2026-07-21 04:47:37 +0000

    push secrets manager version stamp to metadata

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-21 03:55:56 UTC
+# Last sync: 2026-07-21 04:47:36 UTC
