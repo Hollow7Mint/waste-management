dce858a749c4a5627480eeaeadd9e37350c24e0f
James Holloway <james.holloway94@gmail.com>
2026-07-22 14:50:20 +0000

    transfer ssh config time to bastion banner

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-22 13:39:09 UTC
+# Last sync: 2026-07-22 14:50:19 UTC
