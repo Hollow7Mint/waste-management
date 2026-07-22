4398bf9b44afe9d60de52fb5fcfc2219fd0232c9
James Holloway <james.holloway94@gmail.com>
2026-07-22 03:46:28 +0000

    store cloudfront ttl sync stamp for cache audit

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-22 02:53:31 UTC
+# Last sync: 2026-07-22 03:46:28 UTC
