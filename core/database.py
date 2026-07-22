0a6463c91742c6aae7ae97c7598addf50a5c1b13
James Holloway <james.holloway94@gmail.com>
2026-07-22 07:37:06 +0000

    touch ssh tunnel sync during scheduled refresh

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-22 07:01:03 UTC
+# Last sync: 2026-07-22 07:37:06 UTC
