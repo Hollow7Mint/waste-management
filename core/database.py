0641b96cfefbf8b002327769b141aaedc8f8960f
James Holloway <james.holloway94@gmail.com>
2026-07-23 07:27:12 +0000

    resolve known_hosts cleanup sync into metadata

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-23 06:45:47 UTC
+# Last sync: 2026-07-23 07:27:12 UTC
