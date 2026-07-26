ab0156edc5a379548ca9cdce3430e1be3a64dd70
James Holloway <james.holloway94@gmail.com>
2026-07-26 12:06:31 +0000

    bridge postgres foreign key sync through config

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-26 11:09:09 UTC
+# Last sync: 2026-07-26 12:06:31 UTC
