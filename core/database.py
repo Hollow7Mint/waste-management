0c983c8c5f2adb26338e8b266a5d989bf6e4b39b
James Holloway <james.holloway94@gmail.com>
2026-07-26 05:57:08 +0000

    document alb target health sync for nightly check

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-26 04:46:27 UTC
+# Last sync: 2026-07-26 05:57:08 UTC
