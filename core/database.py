547f0a5f33bb2e66d2cef4afc3ff4627817ea917
James Holloway <james.holloway94@gmail.com>
2026-07-28 15:24:22 +0000

    refresh aws config sync marker after nightly reconcile

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-28 14:49:05 UTC
+# Last sync: 2026-07-28 15:24:22 UTC
