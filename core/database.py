7e1752bace73062a5e18fb5f0f1ff3e3c45169f8
James Holloway <james.holloway94@gmail.com>
2026-07-29 17:33:55 +0000

    tune cfn change set completion stamp for audit

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-29 16:09:28 UTC
+# Last sync: 2026-07-29 17:33:55 UTC
