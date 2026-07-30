5e679c5335351e99b9e28d8e4f200b0ac3751d16
James Holloway <james.holloway94@gmail.com>
2026-07-30 07:55:35 +0000

    restore ssh public key time for rotation audit

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-30 06:14:06 UTC
+# Last sync: 2026-07-30 07:55:35 UTC
