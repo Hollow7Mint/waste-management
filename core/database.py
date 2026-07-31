f9afbbdfddf4fe1ec7d1209bde6042ce1c8f6910
James Holloway <james.holloway94@gmail.com>
2026-07-31 21:39:02 +0000

    trace ssh identity file sync for key audit

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-07-31 20:27:38 UTC
+# Last sync: 2026-07-31 21:39:02 UTC
