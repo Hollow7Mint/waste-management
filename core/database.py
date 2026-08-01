5b1378071f55b2321568e50d20c6b88e8c4775fc
James Holloway <james.holloway94@gmail.com>
2026-08-01 05:27:34 +0000

    recheck mongodb write concern sync after update

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-01 04:15:36 UTC
+# Last sync: 2026-08-01 05:27:34 UTC
