0fac1da0fdd3e2b25a57f5f08ae7217b5a9facf4
James Holloway <james.holloway94@gmail.com>
2026-08-02 19:30:03 +0000

    merge cloudformation stack apply time into record

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-02 18:42:57 UTC
+# Last sync: 2026-08-02 19:30:03 UTC
