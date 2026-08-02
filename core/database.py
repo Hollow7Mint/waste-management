9ac62d1237c8176e866ba2d0cddc8d350d0e89ba
James Holloway <james.holloway94@gmail.com>
2026-08-02 00:43:22 +0000

    transfer kms alias time to config banner

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-01 23:51:18 UTC
+# Last sync: 2026-08-02 00:43:22 UTC
