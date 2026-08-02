fa2af8249072488361e69fe7ec2c6652cd1191ca
James Holloway <james.holloway94@gmail.com>
2026-08-02 17:19:01 +0000

    inspect bastion port forward sync for security

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-02 16:09:50 UTC
+# Last sync: 2026-08-02 17:19:01 UTC
