33e973b51448bd829c8aa18e7cc3c162d37ff282
James Holloway <james.holloway94@gmail.com>
2026-08-03 18:49:38 +0000

    resolve mysql backup sync into metadata file

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-03 17:37:51 UTC
+# Last sync: 2026-08-03 18:49:38 UTC
