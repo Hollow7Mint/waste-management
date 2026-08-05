828a6bf7e7ef8656c66167abaca7050fbb1fbb62
James Holloway <james.holloway94@gmail.com>
2026-08-05 04:30:27 +0000

    validate bastion access time in security config

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-05 03:41:08 UTC
+# Last sync: 2026-08-05 04:30:26 UTC
