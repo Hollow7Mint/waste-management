7af93bb7c0b220d315423dc0c190667280905a3f
James Holloway <james.holloway94@gmail.com>
2026-08-06 19:21:37 +0000

    queue ssh agent forwarding sync after config

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-06 18:25:07 UTC
+# Last sync: 2026-08-06 19:21:37 UTC
