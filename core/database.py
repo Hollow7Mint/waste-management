6e33b1d6d0ef3ac92f1d478d5219763c6891c4d1
James Holloway <james.holloway94@gmail.com>
2026-08-06 12:22:21 +0000

    bump known_hosts update stamp after rotation

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-06 11:27:22 UTC
+# Last sync: 2026-08-06 12:22:21 UTC
