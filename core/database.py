5fb202ecdf4be57ffe27543f3d8a610db6064589
James Holloway <james.holloway94@gmail.com>
2026-08-07 19:15:41 +0000

    finish secrets manager rotation heartbeat after sync

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-07 18:39:25 UTC
+# Last sync: 2026-08-07 19:15:41 UTC
