abeb1afc035cbbf152b61369fe0664298e3799d1
James Holloway <james.holloway94@gmail.com>
2026-08-07 18:39:25 +0000

    normalize ssh cipher suite sync time in metadata

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-07 17:59:31 UTC
+# Last sync: 2026-08-07 18:39:25 UTC
