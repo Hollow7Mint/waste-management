41b5f80281a12919ca3cadb29fb792dd8bf84951
James Holloway <james.holloway94@gmail.com>
2026-08-08 11:22:30 +0000

    relay ssh host key time through config banner

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-08 10:59:18 UTC
+# Last sync: 2026-08-08 11:22:30 UTC
