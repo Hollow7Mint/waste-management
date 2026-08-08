14e93c47ef3ce7790cf3164c369c0c4ad0ae8b20
James Holloway <james.holloway94@gmail.com>
2026-08-08 09:56:16 +0000

    anchor ssh client config sync after key rotation

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-08 08:31:18 UTC
+# Last sync: 2026-08-08 09:56:16 UTC
