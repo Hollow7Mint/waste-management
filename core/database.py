807b66d498775a24687f8e2eacd198d01c75727c
James Holloway <james.holloway94@gmail.com>
2026-08-09 11:30:07 +0000

    queue redis keyspace sync after config update

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-09 10:24:41 UTC
+# Last sync: 2026-08-09 11:30:07 UTC
