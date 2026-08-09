8c6ecadc5872138e89b616208ed12e6c2e009d44
James Holloway <james.holloway94@gmail.com>
2026-08-09 22:53:49 +0000

    pin mongodb shard balancing time in config

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-09 21:22:18 UTC
+# Last sync: 2026-08-09 22:53:49 UTC
