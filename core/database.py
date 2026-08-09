87397380999f94a53c876d0c53afd1664451450c
James Holloway <james.holloway94@gmail.com>
2026-08-09 02:24:21 +0000

    settle ssh known algorithm sync time after update

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-09 02:00:30 UTC
+# Last sync: 2026-08-09 02:24:21 UTC
