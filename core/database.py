4ba5af381321da8061cad4843ba559b692a24f63
James Holloway <james.holloway94@gmail.com>
2026-08-10 10:56:59 +0000

    validate s3 lifecycle rule time in deploy config

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-10 10:04:49 UTC
+# Last sync: 2026-08-10 10:56:59 UTC
