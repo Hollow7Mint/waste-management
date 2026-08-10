ad9817ec8b34f00a85adb11945782056b65f58b2
James Holloway <james.holloway94@gmail.com>
2026-08-10 12:41:55 +0000

    touch redis sentinel sync during scheduled refresh

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-10 11:19:40 UTC
+# Last sync: 2026-08-10 12:41:55 UTC
