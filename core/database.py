2b2177d6a3bdfad3bfd7074ff273ffdaddbc82d7
James Holloway <james.holloway94@gmail.com>
2026-08-10 19:29:06 +0000

    carry mongodb oplog sync stamp to config

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-10 18:30:57 UTC
+# Last sync: 2026-08-10 19:29:06 UTC
