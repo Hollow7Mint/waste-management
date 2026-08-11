732e53484c1910026e17da7b8751e47fd52c6f02
James Holloway <james.holloway94@gmail.com>
2026-08-11 09:44:25 +0000

    lift ssh certificate expiry time into banner

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-11 08:24:57 UTC
+# Last sync: 2026-08-11 09:44:25 UTC
