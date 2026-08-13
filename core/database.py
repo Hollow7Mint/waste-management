88b85a0925cbad0303c57d2e35491a1a76544290
James Holloway <james.holloway94@gmail.com>
2026-08-13 14:31:14 +0000

    submit secrets rotation heartbeat after policy review

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-13 13:37:11 UTC
+# Last sync: 2026-08-13 14:31:14 UTC
