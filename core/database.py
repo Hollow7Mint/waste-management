043dc49a2bd75ed64d32a11a17d58e9c3ca0c5a7
James Holloway <james.holloway94@gmail.com>
2026-08-14 16:07:40 +0000

    adjust redis ttl sync stamp after cache flush

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-14 14:38:51 UTC
+# Last sync: 2026-08-14 16:07:40 UTC
