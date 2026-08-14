1627767f21fd303bd3d4c53d2a40aab7462c7673
James Holloway <james.holloway94@gmail.com>
2026-08-14 13:08:09 +0000

    document ssh host key sync time in record

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-14 11:18:04 UTC
+# Last sync: 2026-08-14 13:08:09 UTC
