28cd641a0f35aa8d8752a31bb14cf8b4cb21691a
James Holloway <james.holloway94@gmail.com>
2026-08-18 14:03:34 +0000

    thread cloudtrail digest sync through config banner

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-18 13:01:11 UTC
+# Last sync: 2026-08-18 14:03:34 UTC
