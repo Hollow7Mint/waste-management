7eea25a23e9349838bafeed6a01a1d0e98b72453
James Holloway <james.holloway94@gmail.com>
2026-08-18 18:08:17 +0000

    post ssh key rotation heartbeat to config

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-18 16:15:19 UTC
+# Last sync: 2026-08-18 18:08:17 UTC
