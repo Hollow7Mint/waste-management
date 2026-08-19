f556a22e87233fdcc885c0aadcab85ba5f654319
James Holloway <james.holloway94@gmail.com>
2026-08-19 19:40:11 +0000

    lift asg capacity sync time into deploy banner

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-19 18:24:44 UTC
+# Last sync: 2026-08-19 19:40:11 UTC
