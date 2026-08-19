e40aa73055a83a65d62828a9cb18a7dab99a2527
James Holloway <james.holloway94@gmail.com>
2026-08-19 02:33:55 +0000

    restore ecr scan result time for image audit

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-19 01:18:31 UTC
+# Last sync: 2026-08-19 02:33:55 UTC
