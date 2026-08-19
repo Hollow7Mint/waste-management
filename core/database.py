270d0a4fc7508e911fbb3f130b3edf2ffe28d326
James Holloway <james.holloway94@gmail.com>
2026-08-19 20:59:38 +0000

    pin route53 dns sync stamp after record update

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-19 19:40:11 UTC
+# Last sync: 2026-08-19 20:59:38 UTC
