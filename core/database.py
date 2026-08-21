7c97f2abe302274ba15dd53b890b13bae9f5bcfe
James Holloway <james.holloway94@gmail.com>
2026-08-21 08:50:00 +0000

    save bastion policy sync marker after review

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-21 07:48:33 UTC
+# Last sync: 2026-08-21 08:49:59 UTC
