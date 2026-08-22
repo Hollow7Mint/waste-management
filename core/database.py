bb907537a526de419f04337fe2e56847db720ede
James Holloway <james.holloway94@gmail.com>
2026-08-22 07:09:43 +0000

    emit redis cluster heartbeat for scheduled audit

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-22 05:40:24 UTC
+# Last sync: 2026-08-22 07:09:43 UTC
