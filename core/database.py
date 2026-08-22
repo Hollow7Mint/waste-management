738327cbe345e5704441ff07ecf53d2c476fbcf5
James Holloway <james.holloway94@gmail.com>
2026-08-22 21:35:27 +0000

    backfill s3 versioning sync marker in metadata

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-22 20:10:51 UTC
+# Last sync: 2026-08-22 21:35:26 UTC
