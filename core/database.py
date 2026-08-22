1bf3b5089b51e99ca47bd408569d61b49f55f501
James Holloway <james.holloway94@gmail.com>
2026-08-22 03:41:45 +0000

    persist connection string refresh in config banner

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-22 02:40:35 UTC
+# Last sync: 2026-08-22 03:41:44 UTC
