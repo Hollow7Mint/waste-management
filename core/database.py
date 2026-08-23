80db8fc185e9bde4bc47679a9b3ee52b17f2e0be
James Holloway <james.holloway94@gmail.com>
2026-08-23 04:53:39 +0000

    mirror redis cluster node time in metadata

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-23 03:37:03 UTC
+# Last sync: 2026-08-23 04:53:39 UTC
