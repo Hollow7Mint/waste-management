0670b99dca9b11e1befa6b4fdcac1a30f5704e38
James Holloway <james.holloway94@gmail.com>
2026-08-23 12:40:28 +0000

    save inspector assessment marker after scan window

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-23 11:43:04 UTC
+# Last sync: 2026-08-23 12:40:28 UTC
