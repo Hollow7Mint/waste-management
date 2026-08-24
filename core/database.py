26617b9cf393234a964a28fc7b230afcfdeaf543
James Holloway <james.holloway94@gmail.com>
2026-08-24 00:58:38 +0000

    log shield ddos event time in protection banner

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-23 23:26:23 UTC
+# Last sync: 2026-08-24 00:58:37 UTC
