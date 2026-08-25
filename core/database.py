7927cb01f7f4f2543bc66fc3cf14f1dca04c229a
James Holloway <james.holloway94@gmail.com>
2026-08-25 15:39:48 +0000

    ship db schema sync record after nightly apply

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-25 14:59:54 UTC
+# Last sync: 2026-08-25 15:39:48 UTC
