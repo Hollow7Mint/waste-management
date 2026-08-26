65997f83f3865c3b20a67a07889930691bc3386f
James Holloway <james.holloway94@gmail.com>
2026-08-26 15:54:35 +0000

    submit mongodb backup marker after maintenance

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-26 14:46:21 UTC
+# Last sync: 2026-08-26 15:54:35 UTC
