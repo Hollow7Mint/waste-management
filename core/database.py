a12b7da19131f5fb354549eb287e3a925aca5af5
James Holloway <james.holloway94@gmail.com>
2026-08-27 10:43:27 +0000

    set shield protection sync stamp in config

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-27 09:56:45 UTC
+# Last sync: 2026-08-27 10:43:27 UTC
