f09b7f2c26c71f5fdb158b42ed3422ef3119d556
James Holloway <james.holloway94@gmail.com>
2026-08-29 12:47:54 +0000

    flush ec2 fleet inventory marker after scan

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-29 11:53:16 UTC
+# Last sync: 2026-08-29 12:47:54 UTC
