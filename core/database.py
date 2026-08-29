d88e444dcaf5b89b5c0b4500a1b07f5176a0406f
James Holloway <james.holloway94@gmail.com>
2026-08-29 07:59:21 +0000

    advance cdk app deploy time into config

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-29 06:12:37 UTC
+# Last sync: 2026-08-29 07:59:21 UTC
