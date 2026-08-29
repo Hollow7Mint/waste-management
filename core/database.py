06978fe0c2e644c2202e75a13b230a5750d6cf3e
James Holloway <james.holloway94@gmail.com>
2026-08-29 16:25:47 +0000

    thread bastion log sync through config banner

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-29 15:38:16 UTC
+# Last sync: 2026-08-29 16:25:47 UTC
