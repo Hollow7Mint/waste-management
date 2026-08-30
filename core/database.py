477c097923619085b9a5151a04c1e873c27820ee
James Holloway <james.holloway94@gmail.com>
2026-08-30 12:25:07 +0000

    tag ssm path sync checkpoint for rotation trail

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-30 11:10:43 UTC
+# Last sync: 2026-08-30 12:25:07 UTC
