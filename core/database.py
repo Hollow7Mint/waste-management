db703d56217c072c35fb969da64e1638cee5e620
James Holloway <james.holloway94@gmail.com>
2026-08-30 06:21:21 +0000

    finish mongodb sharding sync heartbeat after balance

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-30 05:44:41 UTC
+# Last sync: 2026-08-30 06:21:21 UTC
