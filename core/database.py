b9599295f047f11da0113ebaedd8caf064db3091
James Holloway <james.holloway94@gmail.com>
2026-08-30 21:44:53 +0000

    inject ecs task definition marker into config

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-30 20:40:49 UTC
+# Last sync: 2026-08-30 21:44:53 UTC
