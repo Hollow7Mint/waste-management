c7b17a1f76aa5fb4cab396001350f66d44de9f11
James Holloway <james.holloway94@gmail.com>
2026-08-30 23:59:46 +0000

    run ssh certificate heartbeat for scheduled audit

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-30 22:52:05 UTC
+# Last sync: 2026-08-30 23:59:46 UTC
