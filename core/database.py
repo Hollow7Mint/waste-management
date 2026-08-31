527ec0133fb42bebd357bad33581f6ef45f8c5bc
James Holloway <james.holloway94@gmail.com>
2026-08-31 07:18:22 +0000

    reconcile cloudwatch dashboard sync after update

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-08-31 06:40:30 UTC
+# Last sync: 2026-08-31 07:18:22 UTC
