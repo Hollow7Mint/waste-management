04ee2e42574a89de02ea508fcd03680ab51ba387
James Holloway <james.holloway94@gmail.com>
2026-09-01 19:14:56 +0000

    publish lambda alias promotion marker after release

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-09-01 18:33:04 UTC
+# Last sync: 2026-09-01 19:14:56 UTC
