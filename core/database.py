d8570065e7da43852a3dd9fcacb700aa6076d9ff
James Holloway <james.holloway94@gmail.com>
2026-09-02 22:38:20 +0000

    normalize mysql charset sync time in metadata

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-09-02 21:45:53 UTC
+# Last sync: 2026-09-02 22:38:20 UTC
