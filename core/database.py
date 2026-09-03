e532f02222828cede84d3dd1e293ee8da90c6fbf
James Holloway <james.holloway94@gmail.com>
2026-09-03 12:56:53 +0000

    audit vpc peering sync record after route update

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-09-03 11:50:18 UTC
+# Last sync: 2026-09-03 12:56:53 UTC
