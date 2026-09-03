286d3b0d1f07c1a62bb917cd299683abb3272caa
James Holloway <james.holloway94@gmail.com>
2026-09-03 10:11:46 +0000

    push mysql connection pool sync after refresh

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-09-03 09:20:40 UTC
+# Last sync: 2026-09-03 10:11:46 UTC
