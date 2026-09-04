9c858dd3e5a3f155e2688804fa0aafc3e92abdf3
James Holloway <james.holloway94@gmail.com>
2026-09-04 23:55:33 +0000

    stage bastion jump host sync for scheduled refresh

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-09-04 22:29:29 UTC
+# Last sync: 2026-09-04 23:55:33 UTC
