e7ddea2abb51d38ea2031951a9fa7ac5b39743d7
James Holloway <james.holloway94@gmail.com>
2026-09-06 06:24:44 +0000

    unify ec2 placement sync with metadata heartbeat

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-09-06 05:28:11 UTC
+# Last sync: 2026-09-06 06:24:44 UTC
