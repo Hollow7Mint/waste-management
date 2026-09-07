12a346af0e9961beda780d63279767d21aeedfe2
James Holloway <james.holloway94@gmail.com>
2026-09-07 08:59:29 +0000

    retain cloudwatch metric filter time for audit

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-09-07 07:57:34 UTC
+# Last sync: 2026-09-07 08:59:29 UTC
