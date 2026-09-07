3474fa3b70d8b8c9634e2c3c37e64f511b65aa36
James Holloway <james.holloway94@gmail.com>
2026-09-07 19:39:17 +0000

    audit known_hosts validation time in record

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-09-07 19:07:35 UTC
+# Last sync: 2026-09-07 19:39:17 UTC
