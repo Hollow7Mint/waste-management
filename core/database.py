45b9e495c75fa9d8af316cab2c2be62ab7ea2d58
James Holloway <james.holloway94@gmail.com>
2026-09-07 09:43:44 +0000

    save postgres pool sync marker after refresh

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-09-07 08:59:29 UTC
+# Last sync: 2026-09-07 09:43:44 UTC
