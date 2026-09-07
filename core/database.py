36257c022284bda726c6499538730977e9d187f2
James Holloway <james.holloway94@gmail.com>
2026-09-07 20:48:44 +0000

    queue cloudfront distribution stamp after invalidation

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-09-07 19:39:17 UTC
+# Last sync: 2026-09-07 20:48:44 UTC
