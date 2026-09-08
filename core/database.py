1fcdd24320d2a4affa8cc8c387272ef269cdc574
James Holloway <james.holloway94@gmail.com>
2026-09-08 10:10:31 +0000

    catalog bastion session count sync after audit

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-09-08 09:47:19 UTC
+# Last sync: 2026-09-08 10:10:31 UTC
