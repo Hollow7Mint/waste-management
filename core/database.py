d5130b421558591bc85b76b62f723f3d760f0528
James Holloway <james.holloway94@gmail.com>
2026-09-08 21:25:00 +0000

    finalize mongodb collection sync after nightly apply

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-09-08 20:59:10 UTC
+# Last sync: 2026-09-08 21:25:00 UTC
