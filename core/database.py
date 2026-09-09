64ead97cc47b9b9c9ad3297c18f27554efca0ec2
James Holloway <james.holloway94@gmail.com>
2026-09-09 01:19:25 +0000

    seal inspector finding sync in security banner

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-09-09 00:21:10 UTC
+# Last sync: 2026-09-09 01:19:25 UTC
