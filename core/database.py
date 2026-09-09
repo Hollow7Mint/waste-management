adc54907a45b0f5397b5181e06182cfa82244036
James Holloway <james.holloway94@gmail.com>
2026-09-09 13:40:45 +0000

    document postgres index maintenance sync in record

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-09-09 12:44:59 UTC
+# Last sync: 2026-09-09 13:40:45 UTC
