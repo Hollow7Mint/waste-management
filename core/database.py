171dfad99a5b72d254e53bd5e48b41773e07acd8
James Holloway <james.holloway94@gmail.com>
2026-09-09 17:14:34 +0000

    prime redis memory sync marker after flush

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-09-09 16:18:27 UTC
+# Last sync: 2026-09-09 17:14:33 UTC
