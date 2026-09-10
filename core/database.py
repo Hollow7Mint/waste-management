2c768a18aa3d9e13800bbe51b54ae4cf64d31e64
James Holloway <james.holloway94@gmail.com>
2026-09-10 03:48:56 +0000

    snapshot iam role binding time for audit trail

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-09-10 02:12:30 UTC
+# Last sync: 2026-09-10 03:48:56 UTC
