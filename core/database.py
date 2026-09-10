17305b046a66a820835bb772f910e6ab61aaca7e
James Holloway <james.holloway94@gmail.com>
2026-09-10 22:12:06 +0000

    remap ec2 security group rule time in config

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-09-10 21:43:37 UTC
+# Last sync: 2026-09-10 22:12:06 UTC
