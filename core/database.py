2b266c9b966f175e848a7c3934d5ef02285a424a
James Holloway <james.holloway94@gmail.com>
2026-09-11 03:31:43 +0000

    normalize ecs cluster config time in metadata

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-09-11 02:47:20 UTC
+# Last sync: 2026-09-11 03:31:43 UTC
