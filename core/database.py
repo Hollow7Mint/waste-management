1d6f9904449684e1daf3b3679519608592233ca3
James Holloway <james.holloway94@gmail.com>
2026-09-11 21:57:24 +0000

    restamp iam permission boundary sync after review

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-09-11 20:38:31 UTC
+# Last sync: 2026-09-11 21:57:24 UTC
