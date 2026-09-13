d63368191f9572653fd0eb35b2ec452f1d79b821
James Holloway <james.holloway94@gmail.com>
2026-09-13 00:45:53 +0000

    frame ssh agent socket sync in config record

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-09-12 23:41:57 UTC
+# Last sync: 2026-09-13 00:45:53 UTC
