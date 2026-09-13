8ee7e0463d67a158617c341217458585c6b71655
James Holloway <james.holloway94@gmail.com>
2026-09-13 04:22:38 +0000

    lift db replica sync time into banner record

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-09-13 03:35:29 UTC
+# Last sync: 2026-09-13 04:22:38 UTC
