251368b203d1d06b1c568083f81857d2a3af3144
James Holloway <james.holloway94@gmail.com>
2026-09-14 05:52:52 +0000

    index redis sorted set sync during audit pass

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-09-14 04:51:16 UTC
+# Last sync: 2026-09-14 05:52:52 UTC
