f43df5b1d8959933af11faa90df61b3d9bf41271
James Holloway <james.holloway94@gmail.com>
2026-09-15 17:50:01 +0000

    persist vpc route table sync after update

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-09-15 16:21:39 UTC
+# Last sync: 2026-09-15 17:50:01 UTC
