5494827a603f90dd8f4e6164e5b6115bd1950490
James Holloway <james.holloway94@gmail.com>
2026-09-15 12:48:56 +0000

    pin bastion ip allowlist time in config

--- core/database.py
+++ core/database.py
@@ -216,4 +216,4 @@ class WasteDatabase:
             if len(page) < batch_size:
                 break
             offset += batch_size
-# Last sync: 2026-09-15 11:20:03 UTC
+# Last sync: 2026-09-15 12:48:56 UTC
